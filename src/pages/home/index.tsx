094dac3fb5deb9fb2c2fecb73943d8cf56390d73
Tran Hoang <tranhoang.finizz@gmail.com>
2024-07-24 00:51:56 +0700

    feat: add redirect to login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,20 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import { withSidebar } from "../../components/ui/sidebar/with-sidebar";
 import { Wrapper } from "./styled";
+import { useNavigate } from "@tanstack/react-router";
+import { useSelector } from "react-redux";
+import { selectToken } from "../../store/user/selector";
 
 const HomeComponent = () => {
+	const token = useSelector(selectToken);
+	const navigator = useNavigate();
+	useEffect(() => {
+		if (token) {
+			navigator({
+				to: "/banner",
+			});
+		}
+	}, [navigator, token]);
 	return (
 		<Wrapper>
 			<h1>Home</h1>
