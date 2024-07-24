93a4fa53ce99a909c8bbf5b83b6b35a8ff3f3eab
Tran Hoang <tranhoang.finizz@gmail.com>
2024-07-24 23:50:56 +0700

    fix: build fail

--- src/pages/news/components/add-modal.tsx
+++ src/pages/news/components/add-modal.tsx
@@ -15,25 +15,6 @@ interface IEditModalProps {
 	onClose: () => void;
 }
 
-const modules = {
-	toolbar: [
-		[{ header: "1" }, { header: "2" }, { font: [] }],
-		[{ size: [] }],
-		["bold", "italic", "underline", "strike", "blockquote"],
-		[
-			{ list: "ordered" },
-			{ list: "bullet" },
-			{ indent: "-1" },
-			{ indent: "+1" },
-		],
-		["link", "image", "video"],
-		["clean"],
-	],
-	clipboard: {
-		// toggle to add extra line breaks when pasting HTML:
-		matchVisual: false,
-	},
-};
 /*
  * Quill editor formats
  * See https://quilljs.com/docs/formats/
